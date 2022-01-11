401454e6f1c69b6a80880fd59ff9a684937159a9
James Killelea <dylan.killelea@yahoo.com>
2022-01-11 15:40:51 -0600

    first commit

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,22 @@
 // Assignment code here
 
 
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  let passwordLength = window.prompt("Choose a Password Length between 8 and 12 characters?");
+
+  let characterType = window.prompt("Please provide the Character Types you would like to include from the following: lowercase, uppercase, numeric, and /or special characters?");
+
+  function generatePassword(){
+
+  };
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  
   passwordText.value = password;
 
 }
